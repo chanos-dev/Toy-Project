25ae03d2f76ae7eaf5769d38300aed9f9b0e7b7f
chanos <chanos.dev@gmail.com>
2020-09-06 17:37:12 +0900

    1. Personal-Project 생성
    2. CryptChan Proejct 추가
    2-1. Form UI 변경
    2-2. TrayIcon 추가

---
+++ CryptChan/CryptChan/Encrypt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptChan
+{
+    public class Encrypt
+    {
+        private Encrypt() { }
+
+        private static Encrypt _instance;
+        
+        public static Encrypt Instance
+        {
+            get
+            {
+                if(_instance is null)
+                {
+                    _instance = new Encrypt();
+                }
+
+                return _instance;
+            }
+        } 
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+---
+### C# Personal Project :) 
+##### 환경 : `Visual Studio 2017`, `Windows 10` 
+---
+1. CrpytChan
